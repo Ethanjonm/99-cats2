d7a47015ad2cd9f97c3d9bd81db75030f524e634
dtannyc1 <dtannyc1@gmail.com>
2023-04-17 15:31:07 -0400

    Finished SessionsController#new and #create, finished views/sessions/new

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        username = params[:user][:username]
+        password = params[:user][:password]
+        @user = User.find_by_credentials(username, password)
+
+        if @user
+            session[:session_token] = @user.reset_session_token!
+            redirect_to cats_url
+        else
+            @user = User.new(username: username)
+            render :new
+        end
+    end
+
+    def destroy
+
+    end
+
+end
