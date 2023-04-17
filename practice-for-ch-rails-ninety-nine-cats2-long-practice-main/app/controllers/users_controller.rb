fcfd713f1ab04888b8471a75c4ed24585a5335aa
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-17 15:07:58 -0400

    added new and create method to users controller, Also added users/new.html.erb

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -6,8 +6,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to cats_url
+        else
+            render :new 
+        end
+        
 
     end
 
+    def user_params
+        params.require(:user).permit(:username, :password)
 
+    end
 end
